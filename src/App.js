e70de29fa52f70508559ff734c6fed8c895bb7a8
pratap22 <sharma.pratap22@gmail.com>
2018-11-19 10:34:01 +0530

    Meta Tags

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Helmet } from 'react-helmet';
 import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -10,12 +9,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Helmet>
-          <meta charSet="utf-8" />
-          <title>My Imagenes</title>
-          <link rel="canonical" href="https://www.myimagens.me" />
-          <meta name="description" content="A portfolio for Pratap Sharma" />
-        </Helmet>
         <Navbar />
         <Body />
         <Footer />
