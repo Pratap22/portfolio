2c88c44ec8d040b4f32af39122391aeb65cd606e
pratap22 <sharma.pratap22@gmail.com>
2018-11-19 12:48:18 +0530

    Meta Tags

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
+import Helmet from 'react-helmet';
 import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Body from './components/Body';
 import './flexbogrid.css';
 
+const metaTags = {
+  title: 'My Imagenes',
+  desc: 'Website for Photographer and Web Developer Pratap Sharma',
+  //url: 'https://www.myimagenes.me/',
+  url: 'http://localhost:3000/',
+  image: '/logo/myImagenes.png'
+};
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Helmet
+          title={metaTags.title}
+          meta={[
+            { name: 'description', content: metaTags.desc },
+            { property: 'og:description', content: metaTags.desc },
+            { property: 'og:title', content: metaTags.title },
+            { property: 'og:url', content: metaTags.url },
+            { property: 'og:image', content: metaTags.image },
+            { itemprop: 'name', content: metaTags.title },
+            { itemprop: 'description', content: metaTags.desc },
+            { itemprop: 'image', content: metaTags.image }
+          ]}
+        />
         <Navbar />
         <Body />
         <Footer />
